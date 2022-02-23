fd0745d7a496d878d99394e58c6dd9102a9b8272
jonas-jasul <jonas.jasul@gmail.com>
2022-02-23 11:23:23 +0200

    added migrations

---
+++ database/migrations/2022_02_23_084306_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            
+            // $table->integer("integer_column");
+            // $table->text("big_text")->nullable();
+            
+            $table->string("genre");
+            $table->string("publisher");
+            //naudoti author_id (int) jei kuriamas separate table
+            //$table->integer("author_id");
+            $table->string("author");
+            $table->dateTime("publ_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
